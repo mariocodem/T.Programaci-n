8f699a05b8e238f3ab0df71406367e9f3cdee596
mariocodem <mariohuertashi@gmail.com>
2025-02-07 12:39:20 -0500

    Actualizacion

--- src/main/java/primerser.java
+++ src/main/java/primerser.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 /**
  *
@@ -42,8 +44,20 @@ public class primerser extends HttpServlet {
             out.println("<h1>Hubo un error, no hay datos ingresados</h1>");
             out.println("</body>");
             out.println("</html>");
-           }else{
+           }else if(autenticarUsuario(login, password)==false){
                out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Administracion de Usuarios</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Este usuario no existe, vuelva a intentarlo</h1>");
+            
+            out.println("</body>");
+            out.println("</html>");
+           
+           }else{
+            out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Administracion de Usuarios</title>");
@@ -53,12 +67,38 @@ public class primerser extends HttpServlet {
             out.println("<p>inicio de sesion exitoso</p>");
             out.println("</body>");
             out.println("</html>");
-           
            }
         }
     }
 
    
+   public static boolean autenticarUsuario(String usuario, String clave) {
+        String archivo = "C:/Users/Mario/OneDrive/Desktop/usuarios.txt";
+ // Archivo donde están los datos
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(":"); // Separar usuario y contraseña
+                if (partes.length == 2) {
+                    String usuarioArchivo = partes[0];
+                    String claveArchivo = partes[1];
+
+                    if (usuarioArchivo.equals(usuario) && claveArchivo.equals(clave)) {
+                        return true; // Usuario y contraseña correctos
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo de usuarios.");
+            e.printStackTrace();
+        }
+
+        return false; // Usuario o contraseña incorrectos
+    }
+   
+        
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
